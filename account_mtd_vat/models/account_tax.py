0132db6cb7fd7fc5258d09179fe221b4e8e0a949
Renzo Brown <renzo@sunflowerweb.nl>
2023-04-04 09:00:47 +0000

    [FIX] quick fix

--- account_mtd_vat/models/account_tax.py
+++ account_mtd_vat/models/account_tax.py
@@ -4,7 +4,7 @@ import logging
 
 from odoo.osv import osv
 from odoo import fields, api, models
-
+logger = logging.getLogger(__name__)
 
 class MtdAccountTaxCode(osv.osv):
     _inherit = "account.tax"
@@ -22,17 +22,20 @@ class MtdAccountTaxCode(osv.osv):
         account_tax_obj = self.env['account.tax'].search([])
 
         for record in account_tax_obj:
-            if record.tag_ids.name == 'PT8M':
-                record.vat_tax_scope = 'PTM'
-            elif record.tag_ids.name == 'PT8R':
-                record.vat_tax_scope = 'PTR'
-            elif record.tag_ids.name == 'PT8MBR':
-                record.vat_tax_scope = 'PTM'
-            elif record.tag_ids.name == 'PT8RBR':
-                record.vat_tax_scope = 'PTR'
-            elif record.tag_ids.name.startswith('ST'): # in ('ST0', 'ST1', 'ST11', 'ST2', 'ST4'):
-                record.vat_tax_scope = 'ST'
-            elif record.tag_ids.name.startswith('PT'): #  in ('PT0', 'PT1', 'PT11', 'PT2', 'PT5', 'PT7', 'PT8'):
-                record.vat_tax_scope = 'PT'
+            try:
+                if 'PT8M' in record.mapped('tag_ids.name'):
+                    record.vat_tax_scope = 'PTM'
+                elif 'PT8R' in record.mapped('tag_ids.name'):
+                    record.vat_tax_scope = 'PTR'
+                 elif 'PT8MBR' in record.mapped('tag_ids.name'):
+                    record.vat_tax_scope = 'PTM'
+                elif 'PT8RBR' in record.mapped('tag_ids.name'):
+                    record.vat_tax_scope = 'PTR'
+                elif  any( [ x and  x.startswith('ST') for x in  record.mapped("tag_ids.name")] ): # in ('ST0', 'ST1', 'ST11', 'ST2', 'ST4'):
+                    record.vat_tax_scope = 'ST'
+                elif  any( [ x and  x.startswith('PT') for x in  record.mapped("tag_ids.name")] ): # in ('PT0', 'PT1', 'PT11', 'PT2', 'PT5', 'PT7', 'PT8'):
+                    record.vat_tax_scope = 'PT'
+            except:
+                logger.info("=======================ERROR  IN PROCESSING: RECORD %s,   TAGIDS %s " % (record.id, record.tag_ids or "NO TAG ID"))
 
         return True
